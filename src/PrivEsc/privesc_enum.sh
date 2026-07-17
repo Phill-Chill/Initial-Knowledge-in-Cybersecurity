bf310ea0def56af6d1e091e92a234cab047caa7c
Philipe Caetano <philipe2015amancio@hotmail.com>
2026-07-17 15:42:03 -0300

    feat(capabilities sweep): adds capability scanning in specififc directories

--- src/PrivEsc/privesc_enum.sh
+++ src/PrivEsc/privesc_enum.sh
@@ -8,31 +8,36 @@ echo ""
 
 # Informações de SO e Kernel
 # Possíveis exploits
-echo "[*] 1. Sistem e Kernel:"
+echo "[*] Sistem e Kernel:"
 uname -a # informações do kernel
 cat /etc/os-release | grep -E "PRETTY_NAME|VERSION" # infromações sobre o SO
 echo ""
 
 
-echo "[*] 2. Identidade e Permissões Sudo:"
+echo "[*] Identidade e Permissões Sudo:"
 id
 echo "Comandos Sudo permitidos:"
 sudo -n -l 2>/dev/null || echo "Sem privilégios de sudo ou precisa de senha."
 echo ""
 
 # Busca de SUID
-echo "[*] 3. Buscando binários com SUID bit ativos:"
+echo "[*] Buscando binários com SUID bit ativos:"
 find / \( -path /mnt -o -path /proc -o -path /sys \) -prune -o -type f -perm -4000 -exec stat -c "%A (%a) %U:%G - %n" {} \; 2>/dev/null | head -n 15
 echo ""
 
+# Busca de capabilities
+echo "[*] Linux capabilities"
+getcap -r /usr/bin /usr/sbin /sbin /bin 2>/dev/null
+echo ""
+
 # Verificaçã de permissão em arquivos críticos
-echo "[*] 4. Verificação de arquivos sensíveis:"
+echo "[*] Verificação de arquivos sensíveis:"
 [ -w /etc/passwd ] && echo "CRÌTICO: /etc/passwd tem permissão de escrita!" || echo "etc/passwd protegido."
 [ -w /etc/shadow ] && echo "CRÍTICO: /etc/shadow tem permissão de escrita!" || echo "etc/shadow protegidp."
 echo ""
 
 # Tarefas agendadas
-echo "[*] 5. Tarefas agendadas:"
+echo "[*] Tarefas agendadas:"
 cat /etc/crontab 2>/dev/null
 ls -la /etc/cron.d/ 2>/dev/null
 echo""
